332c0246947cb439840556ef30d756e289ce2015
2046 <g@ggg.gd>
2022-07-02 18:52:00 +0800

    feat: add logo component

---
+++ components/Logo.vue
@@ -0,0 +1,34 @@
+<style>
+  .logo img {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+<template>
+  <Transform :scale="props.scale ? scale : 1" :origin="props.origin">
+    <div class="logo" :style="style">
+      <img :src="url" />
+    </div>
+  </Transform>
+</template>
+<script setup lang="ts">
+  import { computed, inject } from 'vue'
+  import { injectionSlideScale } from '@slidev/client/constants'
+
+  const props = defineProps<{
+    url: string
+    width: number
+    height: number
+    scale?: boolean
+    origin?: string
+  }>()
+
+  const style = computed(() => {
+    return {
+      width: props.width,
+      height: props.height
+    }
+  })
+
+  const scale = inject(injectionSlideScale)
+</script>
